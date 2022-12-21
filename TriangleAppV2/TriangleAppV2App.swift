86ccad42d7016baf031cc57092e3d7b63acdfb87
septe habudin <septe.vhirgo@gmail.com>
2022-12-21 22:07:29 +0700

    new setup

---
+++ TriangleAppV2/TriangleAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleAppV2App.swift
+//  TriangleAppV2
+//
+//  Created by septe habudin on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TriangleAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
